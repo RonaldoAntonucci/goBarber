6426a5353cd891854b520c1d1eb93c3c1ecfa2da
RonaldoAntonucci <ronaldo.antonuccivrb32@yahoo.com.br>
2019-11-05 10:53:18 -0300

    pequenos ajustes

--- Back/src/app/controllers/ScheduleController.js
+++ Back/src/app/controllers/ScheduleController.js
@@ -1,6 +1,7 @@
 import { startOfDay, endOfDay, parseISO } from 'date-fns';
 import { Op } from 'sequelize';
 
+import User from '../models/User';
 import Appointment from '../models/Appointment';
 
 class ScheduleController {
@@ -16,6 +17,13 @@ class ScheduleController {
           [Op.between]: [startOfDay(parsedDate), endOfDay(parsedDate)],
         },
       },
+      include: [
+        {
+          model: User,
+          as: 'user',
+          attributes: ['name'],
+        },
+      ],
       order: ['date'],
     });
 
