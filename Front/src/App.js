ab5d88483b9544bac362ee5f64b841908a61677f
RonaldoAntonucci <ronaldo.antonuccivrb32@yahoo.com.br>
2019-11-05 11:10:06 -0300

    configuracao inicial de pages e rotas

--- Front/src/App.js
+++ Front/src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import { Router } from 'react-router-dom';
 
 import Routes from './routes';
-import GlobalStyle from './styles/global';
+import history from './services/history';
+// import GlobalStyle from './styles/global';
 
 function App() {
   return (
-    <>
+    <Router history={history}>
       <Routes />
-      <GlobalStyle />
-    </>
+    </Router>
   );
 }
 
