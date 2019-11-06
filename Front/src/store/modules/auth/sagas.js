771c0d3b83cec4ecd31e0522036738a5a5fa24a6
RonaldoAntonucci <ronaldo.antonuccivrb32@yahoo.com.br>
2019-11-06 11:16:15 -0300

    loading do login

--- Front/src/store/modules/auth/sagas.js
+++ Front/src/store/modules/auth/sagas.js
@@ -3,25 +3,29 @@ import { takeLatest, all, call, put } from 'redux-saga/effects';
 import history from '~/services/history';
 import api from '~/services/api';
 
-import { signInSuccess } from './actions';
+import { signInSuccess, signFailure } from './actions';
 
 export function* sigIn({ payload }) {
-  const { email, password } = payload;
+  try {
+    const { email, password } = payload;
 
-  const response = yield call(api.post, 'sessions', {
-    email,
-    password,
-  });
+    const response = yield call(api.post, 'sessions', {
+      email,
+      password,
+    });
 
-  const { token, user } = response.data;
+    const { token, user } = response.data;
 
-  if (!user.provider) {
-    console.tron.error('Usuário nao é provider');
+    if (!user.provider) {
+      console.tron.error('Usuário nao é provider');
+    }
+
+    yield put(signInSuccess(token, user));
+
+    history.push('/dashboard');
+  } catch (err) {
+    yield put(signFailure());
   }
-
-  yield put(signInSuccess(token, user));
-
-  history.push('/dashboard');
 }
 
 export default all([takeLatest('@auth/SIGN_IN_REQUEST', sigIn)]);
