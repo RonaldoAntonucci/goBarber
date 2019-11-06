772cce06a1250ac0e0fab13223877317a335c4de
RonaldoAntonucci <ronaldo.antonuccivrb32@yahoo.com.br>
2019-11-06 10:24:46 -0300

    autenticação configurada

--- Front/src/store/modules/auth/sagas.js
+++ Front/src/store/modules/auth/sagas.js
@@ -1,3 +1,27 @@
-import { all } from 'redux-saga/effects';
+import { takeLatest, all, call, put } from 'redux-saga/effects';
 
-export default all([]);
+import history from '~/services/history';
+import api from '~/services/api';
+
+import { signInSuccess } from './actions';
+
+export function* sigIn({ payload }) {
+  const { email, password } = payload;
+
+  const response = yield call(api.post, 'sessions', {
+    email,
+    password,
+  });
+
+  const { token, user } = response.data;
+
+  if (!user.provider) {
+    console.tron.error('Usuário nao é provider');
+  }
+
+  yield put(signInSuccess(token, user));
+
+  history.push('/dashboard');
+}
+
+export default all([takeLatest('@auth/SIGN_IN_REQUEST', sigIn)]);
