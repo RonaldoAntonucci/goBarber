0c6d4d4434a55e01de8a19f8d7455aa96d2c1b00
RonaldoAntonucci <ronaldo.antonuccivrb32@yahoo.com.br>
2019-11-06 08:43:53 -0300

    pagina de signin e signup

--- Front/src/pages/SignUp/index.js
+++ Front/src/pages/SignUp/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.svg';
 
 export default function SignUp() {
-  return <div />;
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+
+      <form action="">
+        <input placeholder="Nome Completo" />
+        <input type="email" placeholder="Seu e-mail" />
+        <input type="password" placeholder="Sua senha secreta" />
+        <input type="password" placeholder="Confirme sua senha secreta" />
+
+        <button type="submit">Criar Conta</button>
+        <Link to="/">Já tenho login</Link>
+      </form>
+    </>
+  );
 }
